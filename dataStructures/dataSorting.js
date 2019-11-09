36897cf618fbeb155cc27c88bde6b0c7a88fe43a
thomas maestas <thomasm1.maestas@gmail.com>
2019-11-08 23:59:00 -0700

    bubble sort

--- dataStructures/dataSorting.js
+++ dataStructures/dataSorting.js
@@ -53,10 +53,7 @@ newSelectionSort = selectionSort(arraySelectionSort);
 console.log('Selection Sort: '+ newSelectionSort);
 newSelectionSort2 = selectionSort2(arraySelectionSort);
 console.log('Selection Sort 2: '+ newSelectionSort2);
-
-
-
-
+ 
 
 // INSERTION
 function insertionSort(arr){
@@ -75,3 +72,63 @@ let arrayInsertionSort = [0, 2, 34, 22, 2, 1, 9, 76, 4, 10, 19, 17]
 console.log('Unsorted: '+ arrayInsertionSort)
 newInsertionSort = insertionSort(arrayInsertionSort) 
 console.log('Insertion Sort: '+ newInsertionSort);
+
+
+// BUBBLE SORT (UNOPTIMIZED) 
+// function bubbleSort(arr) {
+//     for (var i = arr.length; i > 0; i--) {
+//         for (var j = 0; j < i - 1; j++) {
+//             console.log(arr, arr[j], arr[j + 1]);
+//             if (arr[j] > arr[j + 1]) {
+//                 var temp = arr[j];
+//                 arr[j] = arr[j + 1];
+//                 arr[j + 1] = temp;
+//             }
+//         }
+//     }
+//     return arr;
+// } 
+// ES2015  
+function bubbleSort(arr) {
+    const swap = (arr, idx1, idx2) => {
+        [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+    };
+
+    for (let i = arr.length; i > 0; i--) {
+        for (let j = 0; j < i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr, j, j + 1);
+            }
+        }
+    }
+    return arr;
+}
+ 
+let arrayUnsorted  = [0, 2, 34, 22, 2, 1, 9, 76, 4, 10, 19, 17]
+console.log('Unsorted: '+ arrayUnsorted)
+newBubbleSort = bubbleSort(arrayUnsorted) 
+console.log('Bubble Sort: '+ newBubbleSort);
+
+
+
+// BUBBLE SORT (OPTIMIZED) with noSwaps
+function bubbleSortOptimize(arr){
+  var noSwaps;
+  for(var i = arr.length; i > 0; i--){
+    noSwaps = true;
+    for(var j = 0; j < i - 1; j++){
+      if(arr[j] > arr[j+1]){
+        var temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+        noSwaps = false;         
+      }
+    }
+    if(noSwaps) break;
+  }
+  return arr;
+} 
+let arrayUnsorted2  = [0, 2, 34, 22, 2, 1, 9, 76, 4, 10, 19, 17]
+console.log('Unsorted: '+ arrayUnsorted2)
+newBubbleSort = bubbleSortOptimize(arrayUnsorted2) 
+console.log('Bubble Sort (Optimized): '+ newBubbleSort);
