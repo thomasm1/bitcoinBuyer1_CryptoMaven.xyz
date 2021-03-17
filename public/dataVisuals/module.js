db366a0d6b0b03c3d26204817a3ea22a2ed239ee
thomas maestas <thomasm1.maestas@gmail.com>
2021-03-17 01:26:53 -0400

    d3 colorScale & radiusScale

--- public/dataVisuals/module.js
+++ public/dataVisuals/module.js
@@ -7,7 +7,14 @@ const Module = function (id) {
 const svg = d3.select("#thisSVG");
 const width = +svg.attr("width", "600px");
 const height = +svg.attr("height");
- 
+
+const colorScale = d3.scaleOrdinal() 
+  .domain(['btc','eth','hex'])
+  .range(['yellow','cyan', 'magenta']);
+const radiusScale = d3.scaleOrdinal()
+  .domain(['btc','eth','hex'])
+  .range([50,45,30]);
+
 const render = (selection, {fruits}) => {
   svg
 .style("background", "red");
@@ -18,33 +25,39 @@ const circles = selection.selectAll("circle")
 circles
   .enter()
   .append("circle")
-  .attr("cx", (d, i) => i * 100 + 110)
-  .attr("cy", height / 3)
-  .attr("fill", "darkblue")
-  .attr("r", 50);
+    .attr("cx", (d, i) => i * 100 + 110)
+    .attr("cy", height / 3);
+  // .merge()
+  circles  // circles object IS the update function!!!!!
+  .attr("fill", d => colorScale(d.type))
+  .attr("r", d => radiusScale(d.type))
+    .attr("fill", d => colorScale(d.type))
+    .attr("r", d => radiusScale(d.type));
 
 circles.exit()
   .attr("fill", "black");
 
-svg
-  .selectAll("circle")
-  .data(modules)
+// svg
+//   .selectAll("circle")
+//   .data(modules)
+	};
 
-};
+	const makeModules = (type) => ({ type });
+	const modules = d3.range(4)
+	  .map(() => makeModules("btc"));
 
-const makeModules = (type) => ({ type });
-const modules = d3.range(4)
-  .map(() => makeModules("btc"));
+	render(svg, { modules });
 
-  render(svg, { modules });
+	setTimeout(() => {
+		console.log("hey modules", modules); 
+		modules.pop();
+		render(svg, { modules });
+	}, 1000);
 
-setTimeout(() => {
-  console.log(modules);
-  console.log("hey modules");
-  modules.pop();
-  render(svg, { modules });
-}, 3000);
-
+	setTimeout(() => {  
+		modules[1].type = 'hex';
+		render(svg, { modules });
+	}, 2000);
 }
 let modInstance = new Module("module");
  
