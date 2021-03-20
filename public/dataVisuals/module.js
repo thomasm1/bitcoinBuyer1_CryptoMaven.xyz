9a1fe207855411b9127a4a4b845dbb38a115e329
thomas maestas <thomasm1.maestas@gmail.com>
2021-03-19 23:58:37 -0400

    dynamic text; refactor

--- public/dataVisuals/module.js
+++ public/dataVisuals/module.js
@@ -1,62 +1,105 @@
 // import { render } from './indexModule';
 
 const Module = function (id) {
-    document.getElementById(id).innerHTML = `  
-    <svg id="thisSVG">
+  document.getElementById(id).innerHTML = `  
+    <svg id='thisSVG'>
     </svg>  
     `;
-    
-    
-const svg = d3.select("#thisSVG");
-svg.style("background", "red");
 
-const width = +svg.attr("width", "600px");
-const height = +svg.attr("height");
+  const svg = d3.select('#thisSVG');
+  svg.style('background', 'red');
 
-const colorScale = d3.scaleOrdinal()
-  .domain(["btc", "eth", "hex"])
-  .range(["yellow", "cyan", "magenta"]);
+  const width = +svg.attr('width', '600px');
+  const height = +svg.attr('height');
 
-const radiusScale = d3.scaleOrdinal()
-  .domain(["btc", "eth", "hex"])
-  .range([50, 45, 30]);
+  const colorScale = d3
+    .scaleOrdinal()
+    .domain(['BTC', 'ETH', 'HEX'])
+    .range(['yellow', 'cyan', 'magenta']);
 
-  const render = (selection, { modules }) => {
+  const radiusScale = d3
+    .scaleOrdinal()
+    .domain(['BTC', 'ETH', 'HEX'])
+    .range([50, 45, 30]);
 
-  const circles = selection.selectAll("circle").data(modules);
-  circles
-    .enter().append("circle")
-    .attr("cx", (d, i) => i * 100 + 110)
-    .attr("cy", height / 3)
+  const xPosition = (d, i) => i * 100 + 110;
+
+  const capsule = (selection,  props ) => {
+    const { modules, height } = props;
+    const circles = selection
+    .selectAll('circle')
+    .data(modules, d => d.id);
+
+    circles
+      .enter()
+      .append('circle')
+      .attr('cx', xPosition)
+      .attr('cy', height / 3)
     .merge(circles) // enter+update
-    // circles  // circles object IS the update function!!!!!
-    .attr("fill", (d) => colorScale(d.type))
-    .attr("r", (d) => radiusScale(d.type));
-  circles.exit().attr("fill", "black");  // .remove()
+      // circles  // circles object IS the update function!!!!!
+      .attr('fill', (d) => colorScale(d.type))
+      .attr('cx', xPosition)
+      .attr('r', (d) => radiusScale(d.type));
+
+    circles.exit()
+      .transition().duration(1000)
+      .attr('fill', 'black')
+      .transition().duration(1000)
+      .attr('r', 0)
+      .remove()
+
+    const text = selection.selectAll('text')
+      .data(modules);
+    text.style('text-anchor','middle')
+      .style('color','darkblue');
+    
+    text
+      .enter().append('text')
+      .attr('x', xPosition)
+      .attr('y', height / 3 + 60)
+    .merge(text)  
+      .text(d => d.type);  
+    text.exit().remove()
+  };
+
+  const render = () => {
+    capsule(svg, {
+      modules,
+      height: +svg.attr('height'),
+    });
+  };
+
+  //////////////////////////////////////
+
+  const makeModules = (type) => ({ 
+    type,
+    id: Math.random()
+   }); 
+
+  let modules = d3.range(5).map(() => makeModules('BTC'));
+
+  render();
+
+  setTimeout(() => {
+    console.log('hey modules', modules);
+    modules.pop();
+    render();
+  }, 1000);
+
+  setTimeout(() => {
+    modules[1].type = 'HEX';
+    render();
+  }, 2000);
+
+  setTimeout(() => {
+    modules[2].type = 'ETH';
+    render();
+  }, 3000);
+
+  setTimeout(() => {
+    modules = modules.filter((d,i) => i != 2);
+    render();
+  }, 4000);
 };
 
-//////////////////////////////////////
-
-const makeModules = (type) => ({ type });
-const modules = d3.range(5).map(() => makeModules("btc"));
-
-render(svg, { modules }); 
-
-setTimeout(() => {
-  console.log("hey modules", modules);
-  modules.pop();
-  render(svg, { modules });
-}, 1000);
-
-setTimeout(() => {
-  modules[1].type = "hex";
-  render(svg, { modules });
-}, 2000);
-
-setTimeout(() => {
-  modules[2].type = "eth";
-  render(svg, { modules });
-}, 3000);
-};
-
-let modInstance = new Module("module");
+let modInstance = new Module('module');
