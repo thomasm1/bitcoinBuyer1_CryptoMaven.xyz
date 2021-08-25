bac1b2c6cfb091f494b6e10e6b483b79be971874
Thomas Maestas <thomasm1.maestas@gmail.com>
2021-08-24 20:46:33 -0400

    solidity: 2nd deployments -loto not fixed

--- solidity/inbox/deploy.js
+++ solidity/inbox/deploy.js
@@ -4,8 +4,10 @@
 
  // DEMO WALLET
  const provider = new HDWalletProvider(
-     "sketch mule candy aspect crane coin envelope asset recall acoustic salad write",
-     "https://rinkeby.infura.io/v3/dead3242727049a5afc840c16159efe0"
+    //  "sketch mule candy aspect crane coin envelope asset recall acoustic salad write",
+    //  "https://rinkeby.infura.io/v3/dead3242727049a5afc840c16159efe0"
+         "crack make uncle famous easily emerge recall define video bubble glove cool",
+     "https://rinkeby.infura.io/v3/52ec737bb04b4ebe99029ea5b1cc024a"
      );
 
 const web3 = new Web3(provider);
@@ -18,6 +20,7 @@ const deploy = async () => {
         .deploy({ data: bytecode, arguments: ['Hey There!' ] })
         .send({ gas: '1000000', from: accounts[0] });
 
+        console.log("Interface: ", interface);
     console.log('contract deployed to :', result.options.address);
 };
 deploy();
--- solidity/lottery/deploy.js
+++ solidity/lottery/deploy.js
@@ -4,8 +4,10 @@ const { interface, bytecode } = require("./compile");
 
  // DEMO WALLET
  const provider = new HDWalletProvider(
-  "sketch mule candy aspect crane coin envelope asset recall acoustic salad write",
-  "https://rinkeby.infura.io/v3/dead3242727049a5afc840c16159efe0"
+  // "sketch mule candy aspect crane coin envelope asset recall acoustic salad write",
+  // "https://rinkeby.infura.io/v3/dead3242727049a5afc840c16159efe0"
+    "crack make uncle famous easily emerge recall define video bubble glove cool",
+  "https://rinkeby.infura.io/v3/52ec737bb04b4ebe99029ea5b1cc024a"
 );
 const web3 = new Web3(provider);
 
@@ -16,8 +18,9 @@ const deploy = async () => {
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode })
-    .send({ gas: "1000000", from: accounts[0] });
-  console.log(interface);
-  console.log("Contract deployed to", result.options.address);
+    .send({ gas: '1000000', from: accounts[0] });
+
+        console.log("Interface: ", interface);
+    console.log('contract deployed to :', result.options.address);
 };
 deploy();
