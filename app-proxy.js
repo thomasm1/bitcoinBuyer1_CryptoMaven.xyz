cec29d17ee328f551b8dba58bf6b325b12660efb
Thomas Maestas <thomasm1.maestas@gmail.com>
2021-11-10 23:43:47 -0500

    Added Progressive Web App attributes; Crypto algorithms

--- app-proxy.js
+++ app-proxy.js
@@ -12,7 +12,7 @@ const port = '5000';
     // res.sendFile(path.join(__dirname + '/index.html'), {root: __dirname});
 // });
 
-app.use(express.static('public'));
+app.use(express.static('index'));
  
 
 app.listen(port, () => {
