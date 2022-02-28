0a93710f557485b83fa0840c9a4a0f570cf27c70
Thomas Maestas <thomasm1.maestas@gmail.com>
2022-02-28 00:21:44 -0500

    axios/cheerio DataScraper on /cryptonews

--- app-proxy.js
+++ app-proxy.js
@@ -1,14 +1,40 @@
- 
-import express from 'express';   
- 
-const app= express();
+import axios from "axios";
+import express from "express";
+import   Cheerio   from "cheerio";
+
+const app = express();
 const PORT = process.env.PORT || 5000;
-  
-app.use(express.static('index')); 
 
+// global vars 
+const articles = [];
 
-app.get('/api', (req,res) => {
-    res.json('welcome to CryptoMaven')
-})
+app.use(express.static("index"));
 
-app.listen(PORT, () => setTimeout(function() {console.log("hello")}, 5000)); 
+app.get("/api", (req, res) => {
+  res.json("welcome to CryptoMaven");
+});
+
+// DataScraper to return json data on topics:    MOVE    to DataScrapers   ******
+app.get("/cryptonews", (req, res) => {
+  axios.get("https://www.coindesk.com/tech/").then((response) => {
+    const html = response.data;
+    // console.log(html);
+        const $ = Cheerio.load(html)
+
+        $('a:contains("Ethereum")', html).each(function() {
+            const title = $(this).text()
+            const url = $(this).attr('href')
+            articles.push({
+                title,url
+            })
+            
+        })
+        res.json(articles)
+  }).catch((err) => console.log(err));
+});
+
+app.listen(PORT, () =>
+  setTimeout( () => {
+    console.log(` ... serving on Port ${PORT}`);
+  }, 3500)
+);
