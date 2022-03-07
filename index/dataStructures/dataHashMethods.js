f4fe992d2c4c046c6b3918123204f6ff6fa780b7
Thomas Maestas <thomasm1.maestas@gmail.com>
2022-03-07 17:25:26 -0500

    prototype vs. class cleanups for max/minHeap, BST, hashMethods

--- index/dataStructures/dataHashMethods.js
+++ index/dataStructures/dataHashMethods.js
@@ -1,73 +1,80 @@
 // HashTable
 console.log("%c HASH TABLE: ", "color:white; background-color:orange");
 
-var hash = (string, max) => {
-  var hash = 0;
+
+export function hashMethods(storageLimit = 10) { 
+    this.keyMapArray = [];
+  this.storageLimit = storageLimit; // i.e. maxLength if not specified as arg
+}; // END Hash fx
+
+hashMethods.prototype.hash = function (string, maxLength) {
+  let hashResult = 0;
   for (var i = 0; i < string.length; i++) {
-    hash += string.charCodeAt(i);
+    hashResult += string.charCodeAt(i)-96;
   }
-  return hash % max;
+  return hashResult % maxLength;
 };
 
-let HashMethods = function() {
-  let storage = [];
-  const storageLimit = 4;
-
-  this.print = function() {
-    console.log(storage);
+  hashMethods.prototype.print = function() {
+    console.log(this.keyMapArray);
   };
 
-  this.add = function(key, value) {
-    var index = hash(key, storageLimit);
-    if (storage[index] === undefined) {
-      storage[index] = [[key, value]];
+  hashMethods.prototype.add = function(key, value) {
+    let index = this.hash(key, this.storageLimit);
+    if (this.keyMapArray[index] === undefined) {
+      this.keyMapArray[index] = [[key, value]];
     } else {
       var inserted = false;
-      for (var i = 0; i < storage[index].length; i++) {
-        if (storage[index][i][0] === key) {
-          storage[index][i][1] = value;
+      for (var i = 0; i < this.keyMapArray[index].length; i++) {
+        if (this.keyMapArray[index][i][0] === key) {
+          this.keyMapArray[index][i][1] = value;
           inserted = true;
         }
       }
       if (inserted === false) {
-        storage[index].push([key, value]);
+        this.keyMapArray[index].push([key, value]);
       }
     }
   }; // end add fx
 
-  this.remove = function(key) {
-    var index = hash(key, storageLimit);
-    if (storage[index].length === 1 && storage[index][0][0] === key) {
-      delete storage[index];
+  hashMethods.prototype.remove = function(key) {
+    var index = this.hash(key, this.storageLimit);
+    if (this.keyMapArray[index].length === 1 && this.keyMapArray[index][0][0] === key) {
+      delete this.keyMapArray[index];
     } else {
-      for (var i = 0; i < storage[index]; i++) {
-        if (storage[index][i][0] === key) {
-          delete storage[index][i];
+      for (var i = 0; i < this.keyMapArray[index]; i++) {
+        if (this.keyMapArray[index][i][0] === key) {
+          delete this.keyMapArray[index][i];
         }
       }
     }
   }; // end remove fx
 
-  this.lookup = function(key) {
-    var index = hash(key, storageLimit);
-    if (storage[index] === undefined) {
+  hashMethods.prototype.lookup = function(key) {
+    var index = this.hash(key, this.storageLimit);
+    if (this.keyMapArray[index] === undefined) {
       return undefined;
     } else {
-      for (var i = 0; i < storage[index].length; i++) {
-        if (storage[index][i][0] === key) {
-          return storage[index][i][1];
+      for (var i = 0; i < this.keyMapArray[index].length; i++) {
+        if (this.keyMapArray[index][i][0] === key) {
+          return this.keyMapArray[index][i][1];
         }
       }
     }
   }; //end lookup fx
-}; // END Hash fx
 
-console.log(hash("thomas", 10));
-let ht = new HashTable();
-ht.add("alpha", "one");
-ht.add("beta", "two");
-ht.add("gamma", "three");
-ht.add("delta", "four");
-ht.add("epsilon", "five");
-console.log(ht.lookup("gamma"));
-ht.print();
+  hashMethods.prototype.printCheck = function(){
+
+  console.log(this.hash("thomas", 10));
+  let ht = new hashMethods();
+  ht.add("alpha", "one");
+  ht.add("beta", "two");
+  ht.add("gamma", "three");
+  ht.add("delta", "four");
+  ht.add("epsilon", "five");
+  console.log(ht.lookup("gamma"));
+  ht.print();
+
+  }
+let check = new hashMethods();
+check.printCheck();
