85074154e4f1cfa9634bcf573b5fee2049c8947f
Thomas Maestas <thomasm1.maestas@gmail.com>
2022-03-21 00:41:12 -0400

    Bifurcation of AppGroot Binary Tree a la ELK Stack; react-dom-routing to routes; server to nodeSequelize Dir

---
+++ index/routes/mainRouter.js
@@ -0,0 +1,26 @@
+// Router will perform practically all DOM manipulation, with automatic state change >10-20 frames per second, 
+// so that D3 control feeds data changes, and React/Redux acts as a continuously
+// changing data river, stuttering though it be. 
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Admin from '../components/admin/admin';
+
+
+const MainRouter = () => {
+    return (
+      <>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+             
+            {/* admin  for starters, using Least Privilege, open page access with 2 draw bridges */}
+            <Route path="/admin" element={<Admin />} /> 
+            <Route path="/admin/login" element={<AdminLogin />} />  
+          </Routes>
+        </BrowserRouter>
+      </>
+    );
+  };
+  
+  export default MainRouter;
+  
