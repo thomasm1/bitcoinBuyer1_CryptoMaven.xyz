59416e78cd0e138c8e47e41dc4233d36c6d9c452
Thomas Maestas <thomasm1.maestas@gmail.com>
2022-04-21 01:18:49 -0400

    upgrade to React 18

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import web3 from './web3.js';
+// import web3 from './web3.js';
 
 class App extends React.Component {
   render() {
-    console.log("Web3 version: " + web3.version);
-    web3.eth.getAccounts().then(console.log);
+    // console.log("Web3 version: " + web3.version);
+    // web3.eth.getAccounts().then(console.log);
 
     return (
       <div className="App">
-        
+        <h1>CryptoMaven</h1>
       </div>
     );
   }
