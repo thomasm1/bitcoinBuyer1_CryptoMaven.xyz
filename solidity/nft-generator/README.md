263698189d7d10cf9379545836fdf3b5cb306748
Thomas Maestas <thomasm1.maestas@gmail.com>
2022-11-05 11:38:37 -0400

    solidity: nft-generator started

---
+++ solidity/nft-generator/README.md
@@ -0,0 +1,44 @@
+
+- Source Code https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbkJNZ25NcS1KdERNS1RvOFBydEhwUmhTMXBod3xBQ3Jtc0ttWUpPTXZaVk1lWERERnRWbmkxcDYtLTdGME41bHdZVmNYWmtlYzJnd0xtREVGT2EzNjVGMVZSTFZ1bm0yVWdYZzRsLTFwYXdTU1U3SG9nMEdlVmRNaTJHUDljdEJZTkczN1BkVVRhTUw2ek96TzdXRQ&q=https%3A%2F%2Ffireship.io%2Flessons%2Fweb3-solidity-hardhat-react-tutorial%2F&v=meTpMP0J5E8
+- https://github.com/fireship-io/web3-nft-dapp-tutorial
+
+- Pinata https://pinata.cloud/
+- Hardhat https://hardhat.org/
+- OpenZeppelin Wizard https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbFBqOGpPUnFncFdtMHFuRjVFclVRdWUtOUxtQXxBQ3Jtc0trc3ozNDNwMXVNakliRkpveS04QTJ0Z0s2T0tmbFhnR084Tl9TZFNDSHR5NWtGeGYyV0U0RG1JbkRFRFYzVjJjNm9JN1lMUTJ1TkRoTm1IN3RlTUtjUm9RWGNuZUNQZ243YlNDeWFRWUZyRFowQWxZdw&q=https%3A%2F%2Fdocs.openzeppelin.com%2Fcontracts%2F4.x%2Fwizard&v=meTpMP0J5E8
+
+- Alchemy https://www.alchemy.com/
+- Web3 Criticism https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbFBqOGpPUnFncFdtMHFuRjVFclVRdWUtOUxtQXxBQ3Jtc0trc3ozNDNwMXVNakliRkpveS04QTJ0Z0s2T0tmbFhnR084Tl9TZFNDSHR5NWtGeGYyV0U0RG1JbkRFRFYzVjJjNm9JN1lMUTJ1TkRoTm1IN3RlTUtjUm9RWGNuZUNQZ243YlNDeWFRWUZyRFowQWxZdw&q=https%3A%2F%2Fdocs.openzeppelin.com%2Fcontracts%2F4.x%2Fwizard&v=meTpMP0J5E8
+
+- No-Code NFT tutorial by @codeSTACKr https://youtu.be/AaCgydeMu64
+- https://www.youtube.com/watch?v=meTpMP0J5E8
+
+
+My custom Art Generator Source https://github.com/fireship-io/nft-art-generator
+Hashlips Art Engine   https://github.com/HashLips/hashlips_art_engine
+NFT Generator https://nft-generator.art/
+
+
+
+The demo contains a basic web3 app and smart contract for minting NFTs.
+
+- See it in action in the [Web3 NFT Tutorial](https://youtu.be/meTpMP0J5E8) on YouTube.
+- Follow the full [Web3 Tutorial](https://fireship.io/lessons/web3-solidity-hardhat-react-tutorial) on Fireship.
+
+## Usage
+
+```bash
+git clone <this-repo>
+npm install
+
+# terminal 1
+npx hardhat node
+
+# terminal 2
+npx hardhat compile
+npx hardhat run scripts/sample-script.js --network localhost
+
+# terminal 3 
+npm run dev
+```
+
+Update the deployed contract address in `compoonents/Home.js` 
--- static/js/dataSets.js
+++ static/js/dataSets.js
@@ -5,15 +5,15 @@ function mySet() {
   // collection holding set
   var collection = [];
   // check for presence, return true or false
-  this.has = function(element) {
+  this.has = function (element) {
     return collection.indexOf(element) !== -1;
   };
   //return all els in set
-  this.values = function() {
+  this.values = function () {
     return collection;
   };
   //add el to a set
-  this.add = function(element) {
+  this.add = function (element) {
     if (!this.has(element)) {
       collection.push(element);
       return true; // added
@@ -21,7 +21,7 @@ function mySet() {
     return false; // alread in set
   };
   //remove el from set
-  this.remove = function(element) {
+  this.remove = function (element) {
     if (this.has(element)) {
       var index = collection.indexOf(element);
       collection.splice(index, 1);
@@ -29,27 +29,27 @@ function mySet() {
     }
     return false; // none to remve
   };
-  this.size = function() {
+  this.size = function () {
     return collection.length; // col size
   };
   //  returns the union of two sets
-  this.union = function(otherSet) {
+  this.union = function (otherSet) {
     var unionSet = new Set();
     var firstSet = this.values();
     var secondSet = otherSet.values();
-    firstSet.forEach(function(e) {
+    firstSet.forEach(function (e) {
       unionSet.add(e);
     });
-    secondSet.forEach(function(e) {
+    secondSet.forEach(function (e) {
       unionSet.add(e);
     });
     return unionSet;
   };
   // returns intersection of 2 sets AS A NEW SET
-  this.intersection = function(otherSet) {
+  this.intersection = function (otherSet) {
     var intersectionSet = new mySet();
     var firstSet = this.values();
-    firstSet.forEach(function(e) {
+    firstSet.forEach(function (e) {
       if (otherSet.has(e)) {
         intersectionSet.add(e);
       }
@@ -57,10 +57,10 @@ function mySet() {
     return intersectionSet;
   };
   // returns DIFFERENCE of 2 sets as a new set
-  this.difference = function(otherSet) {
+  this.difference = function (otherSet) {
     var differenceSet = new Set();
     var firstSet = this.values();
-    firstSet.forEach(function(e) {
+    firstSet.forEach(function (e) {
       if (!otherSet.has(e)) {
         differenceSet.add(e);
       }
@@ -68,9 +68,9 @@ function mySet() {
     return differenceSet;
   };
   // tests if set is a subset of a different set
-  this.subset = function(otherSet) {
+  this.subset = function (otherSet) {
     var firstSet = this.values();
-    return firstSet.every(function(value) {
+    return firstSet.every(function (value) {
       return otherSet.has(value);
     });
   };
